d282fb41a452aaa2f8daceeb3f81770b57919343
Carol <anakfig2016@gmail.com>
2024-11-14 15:08:40 -0300

    add: files

---
+++ solution.py
@@ -0,0 +1,31 @@
+def seven_segmentify(time_: str) -> str:
+
+    digitos = {
+        '0': [" _ ", "| |", "|_|"],
+        '1': ["   ", "  |", "  |"],
+        '2': [" _ ", " _|", "|_ "],
+        '3': [" _ ", " _|", " _|"],
+        '4': ["   ", "|_|", "  |"],
+        '5': [" _ ", "|_ ", " _|"],
+        '6': [" _ ", "|_ ", "|_|"],
+        '7': [" _ ", " |", " |"],
+        '8': [" _ ", "|_|", "|_|"],
+        '9': [" _ ", "|_|", " _|"],
+        ':': ["   ", " . ", " . "]
+    }
+
+    if time_[0] == "0":
+        time_ = time_[1:]
+
+    horas, minutos = time_.split(':')
+
+    linha1 = []
+    linha2 = []
+    linha3 = []
+
+    for char in horas + ':' + minutos:
+        linha1.append(digitos[char][0])
+        linha2.append(digitos[char][1])
+        linha3.append(digitos[char][2])
+
+    return '\n'.join([''.join(linha1), ''.join(linha2), ''.join(linha3)])
