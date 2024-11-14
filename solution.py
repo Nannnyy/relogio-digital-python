f5e93975c2c82c0a77ef5e16c7c3ddd7e8becc99
Carol <anakfig2016@gmail.com>
2024-11-14 15:58:19 -0300

    change: value of the '7' key

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def seven_segmentify(time_: str) -> str:
         '4': ["   ", "|_|", "  |"],
         '5': [" _ ", "|_ ", " _|"],
         '6': [" _ ", "|_ ", "|_|"],
-        '7': [" _ ", " |", " |"],
+        '7': [" _ ", "  |", "  |"],
         '8': [" _ ", "|_|", "|_|"],
         '9': [" _ ", "|_|", " _|"],
         ':': ["   ", " . ", " . "],
@@ -19,14 +19,17 @@ def seven_segmentify(time_: str) -> str:
         time_ = " " + time_[1:]
 
     horas, minutos = time_.split(':')
+    print(horas[0])
+    print(horas[1])
+    
 
     linha1 = []
     linha2 = []
     linha3 = []
 
     for char in horas + ':' + minutos:
-            linha1.append(digitos[char][0])
-            linha2.append(digitos[char][1])
-            linha3.append(digitos[char][2])
+        linha1.append(digitos[char][0])
+        linha2.append(digitos[char][1])
+        linha3.append(digitos[char][2])
 
     return '\n'.join([''.join(linha1), ''.join(linha2), ''.join(linha3)])
